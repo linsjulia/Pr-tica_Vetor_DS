31af7a818be6e733a7a517cf0cc140129c05d4d7
Elisa <elisa.silva81@etec.sp.gov.br>
2024-05-20 20:42:46 -0300

    commit

---
+++ atividade vetor/Cliente.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_vetor
+{
+    internal class Cliente
+    {
+     
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public string Prioridade{ get; set; }
+
+        
+
+        public void Cadastrar()
+        {
+            Console.WriteLine("Digite o nome do cliente:");
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade do cliente:");
+            
+            while (true)
+            {
+
+                if (Idade>60)
+                {
+
+                }
+
+                string entrada = Console.ReadLine();
+                try
+                {
+                    Idade = int.Parse(entrada);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Por favor, digite um número inteiro para a idade");
+                }
+            }
+        }
+
+        public void Lista_Prioridade()
+        {
+           if (Idade > 60)
+            {
+                Console.WriteLine("Fila Prioritária: "+Idade);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome}, Idade: {Idade}";
+        }
+    }
+
+
+
+
+}
+    
+
---
+++ atividade vetor/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+                Cliente[] novoCliente = new Cliente[9];
+                string opcao;
+
+                while (true)
+                {
+                    Console.WriteLine("1/ - Cadastrar Cliente - 2/ Fila de Prioridade - 3/ Atender Cliente - 4/ Consultar Fila - Q/ Sair");
+                    opcao = Console.ReadLine().ToUpper();
+
+                    if (opcao == "Q")
+                    {
+
+                    Console.WriteLine("Encerrando o programa...");
+                    break;
+                    }
+
+
+
+                switch (opcao) {
+
+                       
+                        case "1":
+                            for (int i = 0; i < 9; i++)
+                            {
+                                if (novoCliente[i] == null)
+                                {
+                                    novoCliente[i] = new Cliente();
+                                    novoCliente[i].Cadastrar();
+                                    break; 
+                                }
+                            }
+                            break;
+
+                        case "4":
+                            Console.WriteLine("Clientes cadastrados:");
+                            foreach (Cliente procurar in novoCliente)
+                            {
+                                if (procurar != null)
+                                {
+                                    Console.WriteLine(procurar);
+
+                                }
+                            }
+
+                            
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção inválida.");
+                            break;
+                    }
+                }
+
+            }
+
+        }
+
+
+
+        
+        }
+    
+
+
+
